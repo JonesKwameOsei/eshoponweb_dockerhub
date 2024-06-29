6110910c64b39196641a9676ca7967089ded2af4
Jones Osei <oseikwamejones@gmail.com>
2024-06-29 09:11:37 +0100

    Update Program.cs

--- eShopOnWeb-main/src/Web/Program.cs
+++ eShopOnWeb-main/src/Web/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Net.Mime;
+using System.Net.Mime;
 using Ardalis.ListStartupServices;
 using Azure.Identity;
 using BlazorAdmin;
@@ -33,20 +33,36 @@ if (builder.Environment.IsDevelopment() || builder.Environment.EnvironmentName =
 else
 {
     // Configure SQL Server (prod)
-    var credential = new ChainedTokenCredential(new AzureDeveloperCliCredential(), new DefaultAzureCredential());
-    builder.Configuration.AddAzureKeyVault(new Uri(builder.Configuration["AZURE_KEY_VAULT_ENDPOINT"] ?? ""), credential);
-    builder.Services.AddDbContext<CatalogContext>(c =>
+    var keyVaultEndpoint = builder.Configuration["AZURE_KEY_VAULT_ENDPOINT"];
+    if (!string.IsNullOrEmpty(keyVaultEndpoint))
     {
-        var connectionString = builder.Configuration[builder.Configuration["AZURE_SQL_CATALOG_CONNECTION_STRING_KEY"] ?? ""];
-        c.UseSqlServer(connectionString, sqlOptions => sqlOptions.EnableRetryOnFailure());
-    });
-    builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+        var credential = new ChainedTokenCredential(new AzureDeveloperCliCredential(), new DefaultAzureCredential());
+        builder.Configuration.AddAzureKeyVault(new Uri(keyVaultEndpoint), credential);
+    }
+
+    var catalogConnectionStringKey = builder.Configuration["AZURE_SQL_CATALOG_CONNECTION_STRING_KEY"];
+    var identityConnectionStringKey = builder.Configuration["AZURE_SQL_IDENTITY_CONNECTION_STRING_KEY"];
+
+    if (!string.IsNullOrEmpty(catalogConnectionStringKey))
     {
-        var connectionString = builder.Configuration[builder.Configuration["AZURE_SQL_IDENTITY_CONNECTION_STRING_KEY"] ?? ""];
-        options.UseSqlServer(connectionString, sqlOptions => sqlOptions.EnableRetryOnFailure());
-    });
+        builder.Services.AddDbContext<CatalogContext>(c =>
+        {
+            var connectionString = builder.Configuration[catalogConnectionStringKey];
+            c.UseSqlServer(connectionString, sqlOptions => sqlOptions.EnableRetryOnFailure());
+        });
+    }
+
+    if (!string.IsNullOrEmpty(identityConnectionStringKey))
+    {
+        builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+        {
+            var connectionString = builder.Configuration[identityConnectionStringKey];
+            options.UseSqlServer(connectionString, sqlOptions => sqlOptions.EnableRetryOnFailure());
+        });
+    }
 }
 
+// Add services to the container.
 builder.Services.AddCookieSettings();
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
@@ -234,7 +250,6 @@ app.UseRouting();
 app.UseCookiePolicy();
 app.UseAuthentication();
 app.UseAuthorization();
-
 
 app.MapControllerRoute("default", "{controller:slugify=Home}/{action:slugify=Index}/{id?}");
 app.MapRazorPages();
